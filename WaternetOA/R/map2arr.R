783b97caf0f262ad56297c257c162b9aef52d92b
JOlsman <93647998+JOlsman@users.noreply.github.com>
2021-11-03 10:54:46 +0100

    Add files via upload

---
+++ WaternetOA/R/map2arr.R
@@ -0,0 +1,65 @@
+#' Reads Sobek/Delwaq map output files into Array's
+#' @export
+map2arr <- function (filename, timestamp = T, begintime = "1900-01-01 00:00:00"){
+  library("stringr")
+  if (toupper(substr(filename, nchar(filename) - 3, nchar(filename))) !=".MAP") {
+    stop("filename does not seem to be a <.his> file")
+  }
+
+  #filename = "d:/rundir/fr_huidig/delwaq.map"
+  zz <- file(filename, "rb")
+  readChar(zz, 40)
+  readChar(zz, 40)
+  readChar(zz, 40)
+  readChar(zz, 4)
+  timeorigin <- readChar(zz, 19)
+  readChar(zz, 7)
+  scu.prep <- readChar(zz, 8)
+  scu  <- as.numeric(scu.prep) # check for the internal timer
+  sign.scu <- sign(scu)  # sign can be +1, 0 or -1
+
+  ifelse(is.na(sign.scu), dec.sign.scu <- NA,   # NB. no error handling here yet, NA should only occur if timestamp = F and then you don't need dec.sign.scu
+         ifelse(sign.scu == 1 , dec.sign.scu <- "*",
+                ifelse(sign.scu == -1, dec.sign.scu <- "/",
+                       ifelse(sign.scu == 0, stop("The sign of your internal timer is neither negative nor positive, but 0."), stop("Check dec.sign.scu.")))))
+
+  scu.sym  <- readChar(zz, 1)
+  readChar(zz, 1)
+  afm <- readBin(zz, integer(), n = 2)
+
+  syname <- vector("character", afm[1])
+  idump <- seq(1:afm[2])
+  for (i in 1:afm[1]) {
+    syname[i] <- readChar(zz, 20)
+  }
+  loc <- seek(zz)
+  it <- -1
+  itn <- vector("integer", 0)
+  tel <- 0
+
+  while (length(it) > 0) {
+    tel <- tel + 1
+    it <- readBin(zz, integer(), n = 1)
+    if (length(it) > 0) {
+      itn <- c(itn, it)
+      conc <- readBin(zz, "double", n = afm[1] * afm[2],
+                      size = 4)
+      length(conc)
+    }
+  }
+  seek(zz, where = loc)
+  concar <- array(dim = c(length(itn), afm[2], afm[1]))
+  for (i in 1:length(itn)) {
+    it <- readBin(zz, integer(), n = 1)
+    concar[i, , ] <- matrix(readBin(zz, "double", n = afm[1] * afm[2], size = 4), nrow = afm[2], ncol = afm[1], byrow = T)
+  }
+  close(zz)
+  timeorigin <- str_replace_all(timeorigin, "[.]", "-")
+
+
+  ifelse(timestamp, itn2 <- as.character(as.POSIXct(x = sapply(itn, function(x) as.numeric(eval(parse(text = paste(x, dec.sign.scu, scu))))),
+                                                    origin = timeorigin, tz = "GMT")), itn2 <- as.character(as.POSIXct(x = as.character(as.POSIXct(x = as.numeric(itn),
+                                                                                                                                                   origin = begintime, tz = "GMT")))))
+  dimnames(concar) <- list(itn2, str_trim(idump), str_trim(syname))
+  return(concar)
+}
